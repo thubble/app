07be2ae44aab7c9e3c5a66d998bc291f8b8a08f5
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-11-23 10:58:04 -0700

    Updated

--- source/app/web/core/CommandRegistry.cs
+++ source/app/web/core/CommandRegistry.cs
@@ -16,11 +16,7 @@ namespace app.web.core
 
 		public IProcessOneRequest get_the_command_that_can_process(IContainRequestInformation request)
 		{
-			var command = this.commands.FirstOrDefault(c => c.can_handle(request));
-			if (command == null)
-				return fallback;
-
-			return command;
+		  return this.commands.FirstOrDefault(c => c.can_handle(request)) ?? fallback;
 		}
 	}
 }
