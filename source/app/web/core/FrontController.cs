4949786d6187bbf09d642b7e1c29a77d819d0beb
AlexPrince <alexp@iqmetrix.com>
2011-11-23 11:23:37 -0600

    Pushing new changes

--- source/app/web/core/FrontController.cs
+++ source/app/web/core/FrontController.cs
@@ -2,9 +2,15 @@
 {
   public class FrontController : IProcessRequests
   {
+      IFindCommands CommandFinder; 
+
+      public FrontController(IFindCommands finder)
+      {
+          this.CommandFinder = finder;
+      }
     public void process(IContainRequestInformation a_request)
     {
-      throw new System.NotImplementedException();
+        this.CommandFinder.get_the_command_that_can_process(a_request).process(a_request);
     }
   }
 }
