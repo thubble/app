93e7705e9c40ec269825a139603e12b55d8bea72
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-11-24 13:49:15 -0700

    Updated

--- source/app.specs/PageFactorySpecs.cs
+++ source/app.specs/PageFactorySpecs.cs
@@ -1,4 +1,6 @@
-﻿using Machine.Specifications;
+﻿using System.Web;
+using System.Web.UI;
+using Machine.Specifications;
 using app.web.core.aspnet;
 using developwithpassion.specifications.extensions;
 using developwithpassion.specifications.rhinomocks;
@@ -17,18 +19,34 @@ namespace app.specs
     {
       Establish c = () =>
       {
+        the_page = fake.an<IHttpHandler>();
+        the_page_path = "asfasfsa.aspx";
         the_model = new OurViewModel();
+
         page_path_registry = depends.on<IFindPathsToViews>();
+        depends.on<TemplateFactory>((path, type) =>
+        {
+          path.ShouldEqual(the_page_path);
+          type.ShouldEqual(typeof(Page));
+          return the_page;
+        });
+
+        page_path_registry.setup(x => x.find_path_for<OurViewModel>()).Return(the_page_path);
       };
+
       Because b = () =>
-        sut.create_using(the_model);
+        result = sut.create_using(the_model);
 
 
-      It should_get_the_path_to_the_page_that_can_display_the_view_model = () =>
-        page_path_registry.received(x => x.find_path_for<OurViewModel>());
+      It should_create_the_page_for_the_model = () =>
+        result.ShouldEqual(the_page);
+
 
       static IFindPathsToViews page_path_registry;
       static OurViewModel the_model;
+      static string the_page_path;
+      static IHttpHandler the_page;
+      static IHttpHandler result;
     }
 
     public class OurViewModel
---
+++ source/app/web/core/aspnet/TemplateFactory.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace app.web.core.aspnet
+{
+  public delegate object TemplateFactory(string path,Type page_type);
+}
