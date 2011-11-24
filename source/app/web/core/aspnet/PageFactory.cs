a4ec54a926b7ff25c024221f45cf44f82402c57a
Tiernan Hubble <thubble@gmail.com>
2011-11-24 14:22:53 -0600

    Pushing new changes

--- source/app/web/core/aspnet/PageFactory.cs
+++ source/app/web/core/aspnet/PageFactory.cs
@@ -4,9 +4,17 @@ namespace app.web.core.aspnet
 {
   public class PageFactory : ICreateAResponse
   {
-    public IHttpHandler create_using<ReportModel>(ReportModel model)
-    {
-      throw new System.NotImplementedException();
-    }
+  	private IFindPathsToViews page_path_registry;
+
+		public PageFactory(IFindPathsToViews page_path_registry)
+  	{
+			this.page_path_registry = page_path_registry;
+  	}
+
+  	public IHttpHandler create_using<ReportModel>(ReportModel model)
+  	{
+  		this.page_path_registry.find_path_for<ReportModel>();
+  		return null;
+  	}
   }
 }
