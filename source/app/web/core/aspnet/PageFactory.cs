835f864266fc6133d3a567652aff12ccee07f525
Tiernan Hubble <thubble@gmail.com>
2011-11-24 14:53:31 -0600

    Pushing new changes

--- source/app/web/core/aspnet/PageFactory.cs
+++ source/app/web/core/aspnet/PageFactory.cs
@@ -1,20 +1,23 @@
 ﻿using System.Web;
+using System.Web.UI;
 
 namespace app.web.core.aspnet
 {
   public class PageFactory : ICreateAResponse
   {
   	private IFindPathsToViews page_path_registry;
+  	private TemplateFactory template_factory;
 
-		public PageFactory(IFindPathsToViews page_path_registry)
-  	{
+		public PageFactory(IFindPathsToViews page_path_registry, TemplateFactory template_factory)
+		{
 			this.page_path_registry = page_path_registry;
-  	}
+			this.template_factory = template_factory;
+		}
 
   	public IHttpHandler create_using<ReportModel>(ReportModel model)
   	{
-  		this.page_path_registry.find_path_for<ReportModel>();
-  		return null;
+  		string path = this.page_path_registry.find_path_for<ReportModel>();
+  		return template_factory(path, typeof(Page)) as IHttpHandler;
   	}
   }
 }
